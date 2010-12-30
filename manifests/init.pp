037f8e10781942c85dd86f34a30046b16ae07e71
Andy Leonard <andy@hurricane-ridge.com>
2010-12-29 21:08:32 -0800

    f5vpn: Correctly include f5vpn::build.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,17 +6,6 @@ import "classes/*.pp"
 
 class f5vpn {
 
-  # Directory within which to build f5vpn; consider instead
-  # distributing a package for your OS:
-  file { "/root/f5vpn-build":
-    ensure => directory,
-    owner => "root",
-    group => "root",
-    mode => 0700,
-  }
-
-  $src_files = [ "Makefile", "f5vpn-login-runner.c", "f5vpn-login.py", ]
-
-  f5vpn::fetch_source { $src_files: }
+  include f5vpn::build
 
 }
